40d99c4292b1d49d4fc281a330f9d358aa0c5422
Arne Heggestad <arne.heggestad@gmail.com>
2015-01-19 11:13:46 -0500

    pulling facebook ids and tokens from userObj

--- test/data/testUsers.js
+++ test/data/testUsers.js
@@ -1,141 +1,114 @@
+// 10 users overall
+//  - 9 facebook
+//  - 8 twitter
+//  - 6 instagram
+
 module.exports = {
 	'f5ln1svr7ff15y8t': {
 		facebook: {
-			user_id: '',
+			userId: '5893289347784961',
 			token: ''
 		},
 		twitter: {
-			user_id: '',
+			userId: '',
 			token: ''
 		},
 		instagram: {
-			user_id: '',
+			userId: '',
 			token: ''
 		}
 	},
 	'cgf5gowgdz7kwkyb': {
-		facebook: {
-			user_id: '',
-			token: ''
-		},
 		twitter: {
-			user_id: '',
+			userId: '',
 			token: ''
 		},
 		instagram: {
-			user_id: '',
+			userId: '',
 			token: ''
 		}
 	},
 	'1n2z0vto9if56fq9': {
 		facebook: {
-			user_id: '',
-			token: ''
-		},
-		twitter: {
-			user_id: '',
-			token: ''
-		},
-		instagram: {
-			user_id: '',
+			userId: '6171596094652216',
 			token: ''
 		}
 	},
 	'hrlot2b4g36qlsa2': {
 		facebook: {
-			user_id: '',
+			userId: '2369584258470910',
 			token: ''
 		},
 		twitter: {
-			user_id: '',
-			token: ''
-		},
-		instagram: {
-			user_id: '',
+			userId: '',
 			token: ''
 		}
 	},
 	'adtt3oajnq7bjyhn': {
 		facebook: {
-			user_id: '',
+			userId: '0118532916807089',
 			token: ''
 		},
 		twitter: {
-			user_id: '',
+			userId: '',
 			token: ''
 		},
 		instagram: {
-			user_id: '',
+			userId: '',
 			token: ''
 		}
 	},
 	'eicv2s4pyyptv8zu': {
 		facebook: {
-			user_id: '',
+			userId: '1406711616799142',
 			token: ''
 		},
 		twitter: {
-			user_id: '',
+			userId: '',
 			token: ''
 		},
 		instagram: {
-			user_id: '',
+			userId: '',
 			token: ''
 		}
 	},
 	'x6dr5f6bsk2linos': {
 		facebook: {
-			user_id: '',
+			userId: '9041900616835265',
 			token: ''
 		},
 		twitter: {
-			user_id: '',
-			token: ''
-		},
-		instagram: {
-			user_id: '',
+			userId: '',
 			token: ''
 		}
 	},
 	'1axe4gcm4hzgf0ip': {
 		facebook: {
-			user_id: '',
-			token: ''
-		},
-		twitter: {
-			user_id: '',
+			userId: '1481892425484476',
 			token: ''
 		},
 		instagram: {
-			user_id: '',
+			userId: '',
 			token: ''
 		}
 	},
 	'v3lrvwv46duqld0n': {
 		facebook: {
-			user_id: '',
+			userId: '1192714977096056',
 			token: ''
 		},
 		twitter: {
-			user_id: '',
-			token: ''
-		},
-		instagram: {
-			user_id: '',
+			userId: '',
 			token: ''
 		}
 	},
 	'q2b6dwqxz38suxho': {
 		facebook: {
-			user_id: '',
+			userId: '9700134951912602',
 			token: ''
 		},
 		twitter: {
-			user_id: '',
-			token: ''
-		},
-		instagram: {
-			user_id: '',
+			userId: '',
 			token: ''
 		}
 	}
--- test/users.js
+++ test/users.js
@@ -4,9 +4,11 @@ var should = require('should'),
 
 var testUsers = require('./data/testUsers.js');
 
-describe('should parse list of users to be scraped into network-specific objects', function (done) {
-  users.parseUsers(testUsers, function (err, queriesObj) {
-    queriesObj.should.exist;
-    done();
+describe('should parse list of users to be scraped into network-specific objects', function () {
+  it('should find 9 facebook users', function (done) {
+    users.parseUsers(testUsers, function (err, queriesObj) {
+      queriesObj.facebook.length.should.eql(9);
+      done();
+    })    
   })
 })
