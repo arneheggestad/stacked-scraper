60982bc64c552a6e1faae9d90e9f9b84598fb3ca
Arne Heggestad <arne.heggestad@gmail.com>
2015-01-19 13:14:07 -0500

    parsing demo response from twitter into a per-user set of posts

--- test/twitter.js
+++ test/twitter.js
@@ -2,9 +2,22 @@ var should = require('should'),
     twitter = require('../lib/twitter.js')
     ;
 
-define('get tweets', function () {
+var twitterData = require('./data/twitterData.js'),
+    twitterUsers = twitterData.users,
+    goodResponse = twitterData.goodResponse;
+
+describe('get tweets', function () {
   it('should get a user\'s recent tweets', function (done) {
     twitter.getPosts(twitterUsers, function (err, posts) {
+      done();
+    })
+  })
+})
+
+describe('parse a good response', function () {
+  it('should parse good returned data', function (done) {
+    twitter.parseResponse(goodResponse, function (err, parsedPosts) {
+      console.log(parsedPosts);
       done();
     })
   })
