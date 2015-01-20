2ff41bffe08014585d7481985c0f2f9a7a3a3d8b
Arne Heggestad <arne.heggestad@gmail.com>
2015-01-20 11:09:10 -0500

    added test file for routes and sketched routes flow

---
+++ test/routes.js
@@ -0,0 +1,10 @@
+var should = require('should'),
+    routes = require('../lib/routes.js')
+    ;
+
+var testUsers = require('./data/testUsers.js');
+
+describe('routes testing', function () {
+  
+
+})
