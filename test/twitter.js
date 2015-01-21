4f918649d16bd9c154aa31492c58684ca5e20fb5
Arne Heggestad <arne.heggestad@gmail.com>
2015-01-21 11:43:19 -0500

    oauth mod working to get posts

--- test/twitter.js
+++ test/twitter.js
@@ -9,7 +9,8 @@ var twitterData = require('./data/twitterData.js'),
     ;
 
 var secrets = require('../credentials/secrets.js'),
-    validUser = secrets.twitterValid;
+    validUser = {
+      '54b592a9853862b8268e095f': secrets.twitterValid};
 
 describe('twitter: ', function () {
   describe('parse a good response', function () {
@@ -57,41 +58,41 @@ describe('twitter: ', function () {
     })
   })
 
-  describe('generate valid oauth signature', function () {
-    it('should generate an oauth signature', function (done) {
-      twitter.generateSignature(testAuth, function (err, oauthData) {
-        oauthData.hash.should.equal('tnnArxj06cWHq44gCs1OSKk/jLY=');
-        done();
-      })
-    })
-  })
+  // describe('generate valid oauth signature', function () {
+  //   it('should generate an oauth signature', function (done) {
+  //     twitter.generateSignature(testAuth, function (err, oauthData) {
+  //       oauthData.hash.should.equal('tnnArxj06cWHq44gCs1OSKk/jLY=');
+  //       done();
+  //     })
+  //   })
+  // })
 
-  describe('make a nonce', function () {
-    it('should generate a random value', function (done) {
-      twitter.makeNonce(function (nonce) {
-        done();
-      })
-    })
-  })
+  // describe('make a nonce', function () {
+  //   it('should generate a random value', function (done) {
+  //     twitter.makeNonce(function (nonce) {
+  //       done();
+  //     })
+  //   })
+  // })
 
-  describe('generate oauth data for a particular user', function () {
-    it('should generate valid oauth data', function (done) {
-      var testUser = twitterData.singleUser;
-      twitter.generateOauthData(testUser, function (err, signedOauthData) {
-        signedOauthData.hash.should.exist;
-        done();
-      })
-    })
-  })
-  describe('normalized posts: ', function () {
-    // take list of users and build twitter API requests
-    it('should generate twitter API requests for a set of users', function (done) {
-      twitter.buildApiRequests(twitterUsers, function (err, twitterRequests) {
-        twitterRequests.cgf5gowgdz7kwkyb.tokenSecret.should.eql('QCfqI6poTyYpmcoCp1x7VFspBLqU5frkmHaO6unZDrjIS');
-        twitterRequests.hrlot2b4g36qlsa2.consumerKey.should.eql('6NNBDyJ2TavL407A3lWxPFKBI');
-        done();
-      })
-    })
+  // describe('generate oauth data for a particular user', function () {
+  //   it('should generate valid oauth data', function (done) {
+  //     var testUser = twitterData.singleUser;
+  //     twitter.generateOauthData(testUser, function (err, signedOauthData) {
+  //       signedOauthData.hash.should.exist;
+  //       done();
+  //     })
+  //   })
+  // })
+  // describe('normalized posts: ', function () {
+  //   // take list of users and build twitter API requests
+  //   it('should generate twitter API requests for a set of users', function (done) {
+  //     twitter.buildApiRequests(twitterUsers, function (err, twitterRequests) {
+  //       twitterRequests.cgf5gowgdz7kwkyb.tokenSecret.should.eql('QCfqI6poTyYpmcoCp1x7VFspBLqU5frkmHaO6unZDrjIS');
+  //       twitterRequests.hrlot2b4g36qlsa2.consumerKey.should.eql('6NNBDyJ2TavL407A3lWxPFKBI');
+  //       done();
+  //     })
+  //   })
     // make the requests to twitter
     // it('make requests to twitter', function (done) {
     //   twitter.buildApiRequests(twitterUsers, function (err, twitterRequests) {
@@ -103,18 +104,25 @@ describe('twitter: ', function () {
     //   })
     // })
     // build object with normalized posts
-    it('build object with normalized posts', function (done) {
-      done();
-    })
+    // it('build object with normalized posts', function (done) {
+    //   done();
+    // })
     // return object to compiler
-  })
+  // })
   describe('using oauth: ', function () {
-    it('should get posts', function (done) {
-      validUser.url = 'https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=' + validUser.userId;
-      console.log(validUser.url);
-      twitter.getPosts(validUser, function () {
+    it('should get 5 raw posts', function (done) {
+      validUser['54b592a9853862b8268e095f'].url = 'https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=' + validUser['54b592a9853862b8268e095f'].userId + '&count=5';
+      twitter.doOauthQuery(validUser['54b592a9853862b8268e095f'], function (err, rawPosts) {
+        if (err) { console.log(err); }
+        rawPosts.length.should.eql(5);
         done();
-      }) 
+      })
+    })
+  })
+  it('should return normalized posts from twitter', function (done) {
+    twitter.getPosts(validUser, function (err, normalizedPosts) {
+      console.log(normalizedPosts);
+      done();
     })
   })
 })
