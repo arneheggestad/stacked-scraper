bf8eba8a26c84a6f46be980f4a3925ba77033eaa
Arne Heggestad <arne.heggestad@gmail.com>
2015-01-21 15:38:09 -0500

    updated test users to follow current schema

--- test/data/testUsers.js
+++ test/data/testUsers.js
@@ -7,44 +7,44 @@ module.exports = {
 	'f5ln1svr7ff15y8t': {
 		facebook: {
 			userId: '5893289347784961',
-			token: ''
+			accessToken: ''
 		},
 		twitter: {
 			userId: 'fakeStacker1',
-			token: '96448684-4vlHLC1X5PSUZvBkShbdIetoyD5ErFEploGiAiGJU',
+			accessToken: '96448684-4vlHLC1X5PSUZvBkShbdIetoyD5ErFEploGiAiGJU',
 			tokenSecret: 'NRsD3q8yExfgSghXCCNewWsgyCTQ5syiHRKHJAqLJ8F1x',
 			last: '34322408173320824'
 		},
 		instagram: {
 			userId: '',
-			token: ''
+			accessToken: ''
 		}
 	},
 	'cgf5gowgdz7kwkyb': {
 		twitter: {
 			userId: 'fakeStacker2',
-			token: '39849623-URgAnzFqp15SrDimRTL0NplNSFZ3YVwFULEwHFlhp',
+			accessToken: '39849623-URgAnzFqp15SrDimRTL0NplNSFZ3YVwFULEwHFlhp',
 			tokenSecret: 'NRsD3q8yExfgSghXCCNewWsgyCTQ5syiHRKHJAqLJ8F1x'
 		},
 		instagram: {
 			userId: '',
-			token: ''
+			accessToken: ''
 		}
 	},
 	'1n2z0vto9if56fq9': {
 		facebook: {
 			userId: '6171596094652216',
-			token: ''
+			accessToken: ''
 		}
 	},
 	'hrlot2b4g36qlsa2': {
 		facebook: {
 			userId: '2369584258470910',
-			token: ''
+			accessToken: ''
 		},
 		twitter: {
 			userId: 'fakeStacker4',
-			token: '89010384-NwksjAULGcw1QjE4Lnm2IeuWMzPifmM6yrrO5oRY4',
+			accessToken: '89010384-NwksjAULGcw1QjE4Lnm2IeuWMzPifmM6yrrO5oRY4',
 			tokenSecret: 'HXqMgzsTiF7nqqKZ7YvnYpP2A8k0Dov9NxWyDJb16f3An',
 			last: '27006977262674003'
 		}
@@ -52,43 +52,43 @@ module.exports = {
 	'adtt3oajnq7bjyhn': {
 		facebook: {
 			userId: '0118532916807089',
-			token: ''
+			accessToken: ''
 		},
 		twitter: {
 			userId: 'fakeStacker5',
-			token: '34241934-waeOw0lHSwsCTVkqlPbRuXMyj2MkB99ppp5sGINcX',
+			accessToken: '34241934-waeOw0lHSwsCTVkqlPbRuXMyj2MkB99ppp5sGINcX',
 			tokenSecret: 'mdBpbVXPWCy5gTZPzb3g1AZtNCzGWltYlGWkHaj315nWZ',
 			last: '50905826881358466'
 		},
 		instagram: {
 			userId: '',
-			token: ''
+			accessToken: ''
 		}
 	},
 	'eicv2s4pyyptv8zu': {
 		facebook: {
 			userId: '1406711616799142',
-			token: ''
+			accessToken: ''
 		},
 		twitter: {
 			userId: 'fakeStacker6',
-			token: '69842184-ft5tPgWRus2OUzEOfUT0lA2yHyQ6Y3FJlfgzNqPRl',
+			accessToken: '69842184-ft5tPgWRus2OUzEOfUT0lA2yHyQ6Y3FJlfgzNqPRl',
 			tokenSecret: '5M0WPpM9uSX5G4geCv3FkNbvKdamU4zkj4YFkQkshv7Kj',
 			last: '26551958962951798'
 		},
 		instagram: {
 			userId: '',
-			token: ''
+			accessToken: ''
 		}
 	},
 	'x6dr5f6bsk2linos': {
 		facebook: {
 			userId: '9041900616835265',
-			token: ''
+			accessToken: ''
 		},
 		twitter: {
 			userId: 'fakeStacker7',
-			token: '72098011-MJmtmq7SKQNQ8MbzZMQ02ef0qdiQIAPDTHQwSk8Ai',
+			accessToken: '72098011-MJmtmq7SKQNQ8MbzZMQ02ef0qdiQIAPDTHQwSk8Ai',
 			tokenSecret: 'ZeiyD7dFMM0NnNO1IUjTHUhRGieaBaXjkV9eIpYK5tZBb',
 			last: '43345953532726055'
 		}
@@ -96,21 +96,21 @@ module.exports = {
 	'1axe4gcm4hzgf0ip': {
 		facebook: {
 			userId: '1481892425484476',
-			token: ''
+			accessToken: ''
 		},
 		instagram: {
 			userId: '',
-			token: ''
+			accessToken: ''
 		}
 	},
 	'v3lrvwv46duqld0n': {
 		facebook: {
 			userId: '1192714977096056',
-			token: ''
+			accessToken: ''
 		},
 		twitter: {
 			userId: 'fakeStacker9',
-			token: '70120600-hiNkIU8GBMApX9Mc1K4OQv82OYvzPDDBF8Hdw9Yqn',
+			accessToken: '70120600-hiNkIU8GBMApX9Mc1K4OQv82OYvzPDDBF8Hdw9Yqn',
 			tokenSecret: '4VGWW0gpenwD3NDAhRmpoqYhbSdXMsEIOFM8x04tRnydL',
 			last: '92135887066128201'
 		}
@@ -118,11 +118,11 @@ module.exports = {
 	'q2b6dwqxz38suxho': {
 		facebook: {
 			userId: '9700134951912602',
-			token: ''
+			accessToken: ''
 		},
 		twitter: {
 			userId: 'fakeStacker10',
-			token: '68237490-IaHEQ5xAHAGxvz9hf1Nk62XuEHd1ARhuwH6l8Q4Xf',
+			accessToken: '68237490-IaHEQ5xAHAGxvz9hf1Nk62XuEHd1ARhuwH6l8Q4Xf',
 			tokenSecret: 'SO68b8zNxC36K4dzGXQbvGb22ZesHw0gf8h1k42W9XYmq',
 			last: '41289000477788663'
 		}
