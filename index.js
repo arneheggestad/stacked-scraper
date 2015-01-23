321b17feb9bcc690b815f49bf839cb966aac7dfe
Arne Heggestad <arne.heggestad@gmail.com>
2015-01-23 09:55:29 -0500

    refactored all code to work as a module, being passed secrets

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
-// dependencies
-var users = require('./lib/users.js')
-    ;
+// module dependencies
+var async = require('async');
+
+var scraper = function (userObj, secrets, callback) {
+  // method dependencies
+  var users = require('./lib/users.js'),
+      twitter = require('./lib/twitter.js')(secrets),
+      // facebook = require('./lib/facebook.js'),
+      instagram = require('./lib/instagram.js'),
+      collator = require('./lib/collator.js')
+      ;
+
+	// GENERAL FLOW
+  // take userObj, which contains Stacked Sports User IDs and associated networks and token
+  // reconfigure userObj by network
+  users.parseUsers(userObj, function (err, masterQueriesObj) {
+    if (err) { throw (err); } // throw errors for now
+    var twitterQueriesObj = masterQueriesObj.twitter;
+    // var facebookQueriesObj = masterQueriesObj.facebook;
+    var instagramQueriesObj = masterQueriesObj.instagram;
+    // pass appropriate reconfigured userObj to each network
+    async.parallel({
+      twitter: function (asyncCallback) {
+        twitter.getPosts(twitterQueriesObj, function (err, normalizedTwitterPosts) {
+          asyncCallback (null, normalizedTwitterPosts);
+        })
+      },
+      // facebook: function (asyncCallback) {
+      //   // FACEBOOK STUFF GOES HERE
+      //   asyncCallback ();
+      // },
+      instagram: function (asyncCallback) {
+        instagram.getPosts(instagramQueriesObj, function (err, normalizedInstagramPosts) {
+          asyncCallback (null, normalizedInstagramPosts);
+        })
+      }
+    },
+    // async callback:
+      // receive normalized posts from each network
+      // compile all posts into final object by ssUserId
+    function (err, results) {
+      // results should be { twitter: {twitterPosts}, facebook: {facebookPosts}, ... }
+      if (err) {
+        throw (err); // for now; fix this soon
+      } else {
+        // console.log(results);
+        collator.merge(results, function (err, finalPostObject) {
+          if (err) { throw (err); }
+          // return compiled final object
+          return callback (null, finalPostObject);
+        })
+      }
+    })
+  })
+}
+
+module.exports = scraper;
---
+++ test/index.js
@@ -0,0 +1,29 @@
+var should = require('should'),
+    index = require('../index.js')
+    ;
+
+var secrets = require('../credentials/secrets.js'),
+    testUsers = {
+      '54b592a9853862b8268e095f': {
+        twitter: secrets.twitterValid,
+        instagram: secrets.instagramValid
+      },
+      '54b7149858b9aeb81f14fb04': {
+        facebook: secrets.facebookValid
+      }
+    };
+
+describe('index: ', function () {
+  // ultimate test: give object with users and credentials to routes
+  // receive object with users, normalized (sorted) posts, and updated `last` fields
+  index(testUsers, secrets, function (err, collatedPosts) {
+    var posts = collatedPosts[ '54b592a9853862b8268e095f' ].posts;
+    // for (var i = 0; i < posts.length; i++) {
+    //   console.log(posts[i].timestamp);
+    // }
+    it('should return posts in descending timestamp order', function (done) {
+      (posts[0].timestamp > posts[3].timestamp).should.eql(true);
+      done();
+    })
+  })
+})
